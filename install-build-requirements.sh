d39cd10e45a01b4e554bb2316a4e9998800f7d9f
Julian Frohmüller <admin@socialz.de>
2022-08-21 14:10:44 +0200

    Fix build requirements installation script.

--- install-build-requirements.sh
+++ install-build-requirements.sh
@@ -1,4 +1,4 @@
-# install stuff needed for making and building
+
 sudo apt-get install gcc -y
 sudo apt-get install g++ -y
 sudo apt-get install make -y
@@ -17,42 +17,13 @@ sudo gem install fpm
 sudo apt-get install rpm -y
 sudo apt-get install gtk+-2.0 -y
 sudo apt-get install gtk+2.0 -y
-sudo apt install libgtk-3-dev -y
+sudo apt-get install libgtk-3-dev -y
 sudo apt-get install squashfs-tools -y
 sudo apt-get install genisoimage -y
-
-
-# install things for glade
 sudo apt-get install intltool -y
 sudo apt-get install libxml2-dev -y
 sudo apt-get install libgladeui-1-dev -y
 sudo apt-get install libgladeui-dev -y
 sudo apt-get install -y itstool -y
 sudo apt-get install libpango1.0-0 -y
-
-wget http://archive.ubuntu.com/ubuntu/pool/universe/g/glade-3/libgladeui-1-11_3.8.0-0ubuntu6_amd64.deb
-sudo dpkg -i libgladeui-1-11_3.8.0-0ubuntu6_amd64.deb
-rm libgladeui-1-11_3.8.0-0ubuntu6_amd64.deb
-
-wget https://download.gnome.org/sources/glade3/3.8/glade3-3.8.5.tar.xz
-tar xf glade3-3.8.5.tar.xz
-cd glade3-3.8.5
-./configure
-make
-sudo make install
-cd ..
-rm glade3-3.8.5.tar.xz
-sudo rm -r glade3-3.8.5
-
-wget https://download.gnome.org/sources/glade3/3.8/glade3-3.8.6.tar.xz
-tar xf glade3-3.8.6.tar.xz
-cd glade3-3.8.6
-./configure
-make
-sudo make install
-cd ..
-rm glade3-3.8.6.tar.xz
-sudo rm -r glade3-3.8.6
-
-# now install regular up to date glade
-sudo apt install glade -y
+sudo apt-get install glade -y
