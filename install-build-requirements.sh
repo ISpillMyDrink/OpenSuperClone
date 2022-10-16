5fb49659d3647e884b00aa15c25959f0b43d18ae
Julian Frohmüller <admin@socialz.de>
2022-10-16 14:36:31 +0200

    Remove unncessary build requirements from script.

--- install-build-requirements.sh
+++ install-build-requirements.sh
@@ -1,26 +1,11 @@
-
 sudo apt-get install gcc -y
-sudo apt-get install g++ -y
-sudo apt-get install make -y
 sudo apt-get install cmake -y
+sudo apt-get install pkgconf -y
 sudo apt-get install libusb-dev -y
-sudo apt-get install libcurl4-gnutls-dev -y
-sudo apt-get install libssl-dev -y
-sudo apt-get install p7zip-full -y
-sudo apt-get install dh-make -y
-sudo apt-get install devscripts -y
-sudo apt-get install help2man -y
-sudo apt-get install texinfo -y
-sudo apt-get install ruby-dev build-essential -y
-sudo gem install fpm
-sudo apt-get install rpm -y
-sudo apt-get install gtk+-2.0 -y
-sudo apt-get install gtk+2.0 -y
 sudo apt-get install libgtk-3-dev -y
-sudo apt-get install intltool -y
-sudo apt-get install libxml2-dev -y
-sudo apt-get install libgladeui-1-dev -y
-sudo apt-get install libgladeui-dev -y
-sudo apt-get install itstool -y
-sudo apt-get install libpango1.0-0 -y
-sudo apt-get install glade -y
+
+# Necessary for GTK2 build
+# sudo apt-get install libgtk2.0-dev -y
+
+# Necessary for some debug related stuff
+# sudo apt-get install libcurl4-gnutls-dev -y
