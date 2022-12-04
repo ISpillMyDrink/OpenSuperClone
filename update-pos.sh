844bad67eccb4c7fe634b5b29f014983b3f922e0
Julian Frohmüller <admin@socialz.de>
2022-12-04 20:50:16 +0100

    Add script to update PO files and PO templates.

---
+++ update-pos.sh
@@ -0,0 +1,10 @@
+xgettext -d opensuperclone -s -k_ -kN_ -o ./src/opensuperclone/locale/opensuperclone.pot ./src/opensuperclone/*.c
+xgettext -d oscviewer -s -k_ -kN_ -o ./src/oscviewer/locale/oscviewer.pot ./src/oscviewer/*.c
+
+for i in `find ./src/opensuperclone/locale/ -name opensuperclone.po`; do
+    msgmerge -U $i ./src/opensuperclone/locale/opensuperclone.pot --backup=none
+done
+
+for i in `find ./src/oscviewer/locale/ -name oscviewer.po`; do
+    msgmerge -U $i ./src/oscviewer/locale/oscviewer.pot --backup=none
+done
