b9ad0ef587c9923943f7c3311a08b793e8a79398
Julian Frohmüller <admin@socialz.de>
2022-12-07 10:14:33 +0100

    Add installation scripts to ZIP package.

---
+++ res/install/install-dependencies-deb.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Installing dependencies for OpenSuperClone..."
+
+sudo apt-get install libgtk-3-0 -y
+sudo apt-get install libusb-0.1-4 -y
+sudo apt-get install make -y
+sudo apt-get install gcc -y
+
+echo "Done installing dependencies for OpenSuperClone."
